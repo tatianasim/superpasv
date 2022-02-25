da80d1273aba6f56765b4c967ac238965e4ca24f
tatianasim <79799027+tatianasim@users.noreply.github.com>
2022-02-25 11:09:37 +0200

    add users.spec (#6)
    add common.helper
    add config.helper
    add users.helper.js

--- tests/users.spec.js
+++ tests/users.spec.js
@@ -1,5 +1,4 @@
 import UsersHelper from '../helpers/users.helper'
-import ConfigHelper from '../helpers/config.helper'
 import { getRandomItem } from '../helpers/common.helper'
 import { expect } from 'chai'
 
@@ -12,10 +11,7 @@ describe('Users', function () {
         userId = userHelper.response.body.id
     })
 
-    after(async function() {
-        const configHelper = new ConfigHelper()
-        await configHelper.wipeData()
-    })
+
 
     describe('User creation', function() {
         it('response status code is 200', function() {
